4af33e257cf2fd306656c3fb915a034be0f9239c
raj2004halder <rajhalder455@gmail.com>
2025-07-06 18:25:48 +0000

    container done

--- .devcontainer/01_basics/01_variables.js
+++ .devcontainer/01_basics/01_variables.js
@@ -1,1 +1,7 @@
-const 
+const acountId = 1234556
+let accountEmail = "raj@google.com"
+var accountPassword = "12366"
+accountCity ="kalyani"
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
